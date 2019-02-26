52a80fd83393eb8b405a7635a4405e49b05d6cba
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-02-25 19:19:31 -0800

    Add snow level data

---
+++ src/components/assets/data.js
@@ -0,0 +1,154 @@
+export const DataSet = [
+  {
+    year: 1981,
+    level: 106.3
+  },
+  {
+    year: 1982,
+    level: 424.4
+  },
+  {
+    year: 1983,
+    level: 306.6
+  },
+  {
+    year: 1984,
+    level: 361.5
+  },
+  {
+    year: 1985,
+    level: 486.6
+  },
+  {
+    year: 1986,
+    level: 286.3
+  },
+  {
+    year: 1987,
+    level: 270.1
+  },
+  {
+    year: 1988,
+    level: 272.9
+  },
+  {
+    year: 1989,
+    level: 420.8
+  },
+  {
+    year: 1990,
+    level: 324.2
+  },
+  {
+    year: 1991,
+    level: 335.5
+  },
+  {
+    year: 1992,
+    level: 183.3
+  },
+  {
+    year: 1993,
+    level: 279.6
+  },
+  {
+    year: 1994,
+    level: 232.0
+  },
+  {
+    year: 1995,
+    level: 363.9
+  },
+  {
+    year: 1996,
+    level: 250.5
+  },
+  {
+    year: 1997,
+    level: 520.6
+  },
+  {
+    year: 1998,
+    level: 257.9
+  },
+  {
+    year: 1999,
+    level: 482.6
+  },
+  {
+    year: 2000,
+    level: 339.3
+  },
+  {
+    year: 2001,
+    level: 178.0
+  },
+  {
+    year: 2002,
+    level: 423.2
+  },
+  {
+    year: 2003,
+    level: 179.3
+  },
+  {
+    year: 2004,
+    level: 289.8
+  },
+  {
+    year: 2005,
+    level: 73.2
+  },
+  {
+    year: 2006,
+    level: 292.5
+  },
+  {
+    year: 2007,
+    level: 258.8
+  },
+  {
+    year: 2008,
+    level: 443.2
+  },
+  {
+    year: 2009,
+    level: 297.0
+  },
+  {
+    year: 2010,
+    level: 260.5
+  },
+  {
+    year: 2011,
+    level: 341.6
+  },
+  {
+    year: 2012,
+    level: 319.3
+  },
+  {
+    year: 2013,
+    level: 244.0
+  },
+  {
+    year: 2014,
+    level: 237.7
+  },
+  {
+    year: 2015,
+    level: 74.2
+  },
+  {
+    year: 2016,
+    level: 199.1
+  },
+  {
+    year: 2017,
+    level: 255.8
+  },
+  {
+    year: 2018,
+    level: 182.4,
+  }
+];
